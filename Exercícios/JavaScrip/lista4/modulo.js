143821d88e361cd41ede6725931195fa15cc0ee0
Ariel <tocadaraposaretro@gmail.com>
2024-08-29 14:38:43 -0300

    atualização

---
+++ Exercícios/JavaScrip/lista4/modulo.js
@@ -0,0 +1,11 @@
+function somaLista(lista) {
+    let total = 0;
+    let N = lista.longth;
+
+    for (let i= 0; i<N; i++) {
+        total += lista[i];
+    }
+    return total
+}
+
+module.exports = somaLista;
