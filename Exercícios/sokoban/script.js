7011a7802638efbcc43999b15191d65a75e76c3d
Ariel <tocadaraposaretro@gmail.com>
2024-09-16 14:16:14 -0300

    os dois cubos conpletos

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,7 +1,24 @@
-const game = document.getElementById ('game')
+const tabuleiro = document.getElementById('board2');
 
 
+function criarLinha(Celulas) {
+    const linha = document.createElement('div');
+    linha.classList.add('row');
 
+    for (let i = 0; i < Celulas; i++) {
+        const celula = document.createElement('div');
+        celula.classList.add('cell');
+        linha.append(celula);
+    }
 
+    return linha;
+}
 
-console.log(funsionou)
+const numLinhas = 4; 
+const numCelulasPorLinha = 4; 
+
+for (let i = 0; i < numLinhas; i++) {
+    const linha = criarLinha(numCelulasPorLinha);
+    tabuleiro.append(linha);
+}
+
