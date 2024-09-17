fe206d038d858e54c8460f14fab31e8d509b6db6
Ariel <tocadaraposaretro@gmail.com>
2024-09-17 14:02:19 -0300

    cubo co o meio vasio

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,24 +1,57 @@
 const tabuleiro = document.getElementById('board2');
 
-
-function criarLinha(Celulas) {
+function criarLinha(Celulas, linhaIndex) {
     const linha = document.createElement('div');
     linha.classList.add('row');
 
     for (let i = 0; i < Celulas; i++) {
         const celula = document.createElement('div');
         celula.classList.add('cell');
+
+        
+        if (linhaIndex >= 1 && linhaIndex <= 6 && i >= 1 && i <= 6) {
+            celula.classList.add('empty'); 
+            celula.style.backgroundColor = 'transparent';
+        }
+
         linha.append(celula);
     }
 
     return linha;
 }
 
-const numLinhas = 4; 
-const numCelulasPorLinha = 4; 
+const numLinhas = 8; 
+const numCelulasPorLinha = 8;
 
 for (let i = 0; i < numLinhas; i++) {
-    const linha = criarLinha(numCelulasPorLinha);
+    const linha = criarLinha(numCelulasPorLinha, i);
     tabuleiro.append(linha);
 }
 
+
+
+
+// const tabuleiro = document.getElementById('board2');
+
+
+// function criarLinha(Celulas) {
+//     const linha = document.createElement('div');
+//     linha.classList.add('row');
+
+//     for (let i = 0; i < Celulas; i++) {
+//         const celula = document.createElement('div');
+//         celula.classList.add('cell');
+//         linha.append(celula);
+
+//     }
+
+//     return linha;
+// }
+
+// const numLinhas = 8; 
+// const numCelulasPorLinha = 8;
+
+// for (let i = 0; i < numLinhas; i++) {
+//     const linha = criarLinha(numCelulasPorLinha);
+//     tabuleiro.append(linha);
+// }
