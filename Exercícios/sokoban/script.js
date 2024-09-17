7364f93eedf90c396cf4ad4aae89d7dbab07e38f
Ariel <tocadaraposaretro@gmail.com>
2024-09-17 14:57:52 -0300

    X incompleto

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,6 +1,9 @@
+
+
+//X incompleto  
 const tabuleiro = document.getElementById('board2');
 
-function criarLinha(Celulas, linhaIndex) {
+function criarLinha(Celulas, linhaIndex,linhaIndex2) {
     const linha = document.createElement('div');
     linha.classList.add('row');
 
@@ -9,14 +12,19 @@ function criarLinha(Celulas, linhaIndex) {
         celula.classList.add('cell');
 
         
-        if (linhaIndex >= 1 && linhaIndex <= 6 && i >= 1 && i <= 6) {
-            celula.classList.add('empty'); 
+        if (i === linhaIndex || i === Celulas -  - linhaIndex) {
+
             celula.style.backgroundColor = 'transparent';
+            celula.style.border = 'none';
+        } else {
+
+            celula.classList.add('not-x-part');
         }
 
         linha.append(celula);
     }
 
+    
     return linha;
 }
 
@@ -31,6 +39,42 @@ for (let i = 0; i < numLinhas; i++) {
 
 
 
+// cubo com o meio vasio {
+// const tabuleiro = document.getElementById('board2');
+
+// function criarLinha(Celulas, linhaIndex) {
+//     const linha = document.createElement('div');
+//     linha.classList.add('row');
+
+//     for (let i = 0; i < Celulas; i++) {
+//         const celula = document.createElement('div');
+//         celula.classList.add('cell');
+
+        
+//        if (linhaIndex >= 1 && linhaIndex <= 6 && i >= 1 && i <= 6) {
+//         celula.classList.add('empty'); 
+//         celula.style.backgroundColor = 'transparent';
+//          } 
+
+//         linha.append(celula);
+//     }
+
+//     return linha;
+// }
+
+// const numLinhas = 8; 
+// const numCelulasPorLinha = 8;
+
+// for (let i = 0; i < numLinhas; i++) {
+//     const linha = criarLinha(numCelulasPorLinha, i);
+//     tabuleiro.append(linha);
+// }
+
+//}
+
+
+
+ //tabuleiro completo
 // const tabuleiro = document.getElementById('board2');
 
 
