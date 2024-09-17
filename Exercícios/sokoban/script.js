37c42f114fd25d47eb66e0b35c2fd5af99456260
Ariel <tocadaraposaretro@gmail.com>
2024-09-17 16:54:49 -0300

    grandes problemas pra resouver

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,45 +1,49 @@
-
-
-//X incompleto  
-const tabuleiro = document.getElementById('board2');
-
 function criarLinha(Celulas, linhaIndex,linhaIndex2) {
     const linha = document.createElement('div');
     linha.classList.add('row');
 
-    for (let i = 0; i < Celulas; i++) {
-        const celula = document.createElement('div');
-        celula.classList.add('cell');
+    Callback (Celulas, numRows, numCols, k, i);
+}
+//X incompleto  
+// const tabuleiro = document.getElementById('board2');
+
+// function criarLinha(Celulas, linhaIndex,linhaIndex2) {
+//     const linha = document.createElement('div');
+//     linha.classList.add('row');
+
+//     for (let i = 0; i < Celulas; i++) {
+//         const celula = document.createElement('div');
+//         celula.classList.add('cell');
 
         
-        if (i === linhaIndex || i === Celulas -  - linhaIndex) {
+//         if (i === linhaIndex || i === Celulas -  - linhaIndex) {
 
-            celula.style.backgroundColor = 'transparent';
-            celula.style.border = 'none';
-        } else {
+//             celula.style.backgroundColor = 'transparent';
+//             celula.style.border = 'none';
+//         } else {
 
-            celula.classList.add('not-x-part');
-        }
+//             celula.classList.add('not-x-part');
+//         }
 
-        linha.append(celula);
-    }
+//         linha.append(celula);
+//     }
 
     
-    return linha;
-}
+//     return linha;
+// }
 
-const numLinhas = 8; 
-const numCelulasPorLinha = 8;
+// const numLinhas = 8; 
+// const numCelulasPorLinha = 8;
 
-for (let i = 0; i < numLinhas; i++) {
-    const linha = criarLinha(numCelulasPorLinha, i);
-    tabuleiro.append(linha);
-}
-
-
+// for (let i = 0; i < numLinhas; i++) {
+//     const linha = criarLinha(numCelulasPorLinha, i);
+//     tabuleiro.append(linha);
+// }
 
 
-// cubo com o meio vasio {
+
+
+// // cubo com o meio vasio 
 // const tabuleiro = document.getElementById('board2');
 
 // function criarLinha(Celulas, linhaIndex) {
@@ -70,7 +74,7 @@ for (let i = 0; i < numLinhas; i++) {
 //     tabuleiro.append(linha);
 // }
 
-//}
+
 
 
 
