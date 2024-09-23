db443789c1a973aebefd2f77aadc89631b1aa0eb
Ariel <tocadaraposaretro@gmail.com>
2024-09-23 13:55:05 -0300

    consegui arumar o codigo java😝😝😝

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,43 +1,28 @@
 function Player(posX, posY) {
    this.x = posX;
    this.y = posY;
+
+   this.move = function(keycode, cells, playerElement) {
+       if (keycode === "ArrowUp") this.x--;
+       if (keycode === "ArrowDown") this.x++;
+       if (keycode === "ArrowLeft") this.y--;
+       if (keycode === "ArrowRight") this.y++;
+       
+       console.log(keycode, this);
+       const K = this.x * 8 + this.y;
+       cells[K].append(playerElement);
+   };
+
+   this.verifyPosition = function() {
+       return this.x >= 0 && this.x < 4 && this.y >= 0 && this.y < 4;
+   };
 }
+
 const player = new Player(0, 0);
 const cells = document.querySelectorAll('.cell');
-console.log(cells);
-
 const jogador = document.querySelector('.jogador');
-console.log(jogador);
-
-jogador.addEventListener("click", function () {
-   window.alert("clicou no jogador");
-})
 
 window.addEventListener("keydown", function (event) {
-   // console.log(event);
    console.log(event.code);
-   // console.log(event.ctrlKey);
-   nextPositon(event.code);
-})
-
-function nextPositon(keycode) {
-   if (keycode == "ArrowUp") player.x--;
-
-   if (keycode == "ArrowDown") player.x++;
-
-   if (keycode == "ArrowLeft") player.y--;
-
-   if (keycode == "ArrowRight") player.y++;
-
-   console.log(keycode, player);
-   const K = player.x * 8 + player.y;
-   cells[K].append(jogador);
-}
-
-function movePlayer(Positon){
-
-}
-
-function verifyPositon(Positon) {
-
-}
+   player.move(event.code, cells, jogador);
+});
