a9e61e7ee8afac1f1df91d9d4f5a7330b5ebe7bb
Ariel <tocadaraposaretro@gmail.com>
2024-09-25 13:59:34 -0300

    25/09/2024

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,3 +1,20 @@
+const player = new Player(0, 0);
+const cells = document.querySelectorAll('.cell');
+const jogador = document.querySelector('.jogador');
+
+const DIST_SALTO = 66;
+const MARGIN_FIX = 4;
+
+jogador.style.top = "64px"
+jogador.style.left = "64px"
+
+
+window.addEventListener("keydown", function (event) {
+   console.log(event.code);
+   player.move(event.code, cells, jogador);
+});
+
+
 function Player(posX, posY) {
    this.x = posX;
    this.y = posY;
@@ -10,7 +27,8 @@ function Player(posX, posY) {
        
        console.log(keycode, this);
        const K = this.x * 8 + this.y;
-       cells[K].append(playerElement);
+
+     
    };
 
    this.verifyPosition = function() {
@@ -18,11 +36,12 @@ function Player(posX, posY) {
    };
 }
 
-const player = new Player(0, 0);
-const cells = document.querySelectorAll('.cell');
-const jogador = document.querySelector('.jogador');
+function calculaPosicao(qtd) {
+   return  `${qtd + DIST_SALTO + MARGIN_FIX } px`;
+}
 
-window.addEventListener("keydown", function (event) {
-   console.log(event.code);
-   player.move(event.code, cells, jogador);
-});
+console.log(calculaPosicao(0, 64) === "0px");
+console.log(calculaPosicao(1, 64) === "64px");
+console.log(calculaPosicao(2, 32) === "64px");
+console.log(calculaPosicao(10, 60) === "600px");
+console.log(calculaPosicao(-3, 45) === "-135px");
