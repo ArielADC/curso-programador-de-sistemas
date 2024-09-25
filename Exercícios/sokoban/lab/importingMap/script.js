6e0284e529d0d07dee4c9120e09b2ac5578b7daa
Ariel <tocadaraposaretro@gmail.com>
2024-09-25 17:09:57 -0300

    cria tabuleiro diferensiado 👍👍👍

---
+++ Exercícios/sokoban/lab/importingMap/script.js
@@ -0,0 +1,43 @@
+const boardMap = [
+   [ "#", "#", "#", "#", "#", "#", "#", "#" ],
+   [ "#", ".", ".", ".", ".", ".", ".", "#" ],
+   [ "#", ".", ".", ".", "#", ".", ".", "#" ],
+   [ "#", ".", "#", "G", ".", ".", ".", "#" ],
+   [ "#", ".", ".", "G", "B", "#", ".", "#" ],
+   [ "#", ".", ".", "#", ".", "B", ".", "#" ],
+   [ "#", ".", ".", "P", ".", ".", ".", "#" ],
+   [ "#", "#", "#", "#", "#", "#", "#", "#" ]
+]
+
+
+const NUM_ROWS = boardMap.length;
+const NUM_COLS = boardMap[0].length;
+
+buildGameBoard(NUM_ROWS, NUM_COLS);
+
+
+function createGameElement(elementName, className, parentNode) {
+   const playerElement = document.createElement(elementName);
+   playerElement.classList.add(className);
+   parentNode.append(playerElement);
+
+   return playerElement;
+}
+
+function buildGameBoard(numRows, numCols) {
+   const game = document.getElementById("game");
+   const board = createGameElement('div', 'board', game);
+   console.log(numCols,numRows);
+
+   for (let x = 0; x < numRows; x++) {
+      const row = createGameElement('div', 'row', board);
+     
+      for (let y = 0; y < numCols; y++) {
+         const cell = createGameElement('div', 'cell', row);
+         const char = boardMap[x][y];
+
+         if (char === '#')cell.classList.add('wall');
+      }
+   }
+
+}
