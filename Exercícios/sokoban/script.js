9499b9fc0774bbdd3c05a6aff6551b0082d986f7
Ariel <tocadaraposaretro@gmail.com>
2024-10-01 16:38:27 -0300

    movimentação de caixas completa

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -21,36 +21,36 @@ for (let i = 0; i < pieces.block.length; i++)  {
 }
 
 window.addEventListener("keydown", function (event) {
-    event.preventDefault();
+    // event.preventDefault();
 
     handlePieceMovement(event.code);
 });
 
-/* Tarefa #1: implementar função para localizar uma caixa à partir de um
- uma dada coordenada.
-*/
+
 function findBoxAtPosition(position) {
-    // modificar linha(s) de código abaixo
-    return null;
+    return boxes.find((caixa) => caixa.x === position.x && caixa.y === position.y);
 }
 
-/** Tarefa #2: modificar a função abaixo de forma a tratar tando a movimentação
- * do jogador quanto das caixas.
-*/
-function handlePieceMovement(keycode){
-    // Variável destinada ao pré-cálculo da posição do jogador
+console.log(findBoxAtPosition({x: 1, y: 2}));
+console.log(findBoxAtPosition({x: 4, y: 4}));
+console.log(findBoxAtPosition({x: 4, y: 5}));
+console.log(findBoxAtPosition({x: 5, y: 5}));
+
+
+function handlePieceMovement(keycode) {
     const next = player.nextPosition(keycode);
-    // (Modificar) Variável para detectar a "presença" de outra peça
-    const foundPice = null;
+    
+    const foundPiece = findBoxAtPosition(next);
 
-    // Implementar lógica caso encontre uma outra peça no caminho.
-    if(foundPice) {
+    if (foundPiece) {
+        const boxNextPosition = foundPiece.nextPosition(keycode);
 
+        if (verifyPosition(boxNextPosition) && !findBoxAtPosition(boxNextPosition)) {
+            foundPiece.moveTo(boxNextPosition);
+            player.moveTo(next);
+        }
     }
-    // E caso não encontre outra peça...
     else {
-        // Faça as modificações que forem necessárias para manter o
-        // funcionamento do jogo.
         if (verifyPosition(next)) {
             player.moveTo(next);
         }
