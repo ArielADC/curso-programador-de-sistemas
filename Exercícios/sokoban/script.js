beda17c5e8ed5c006124192b4d8c827217ac7f89
Ariel <tocadaraposaretro@gmail.com>
2024-10-02 13:48:20 -0300

    estajos finais

--- Exercícios/sokoban/script.js
+++ Exercícios/sokoban/script.js
@@ -1,4 +1,4 @@
-import {buildGameBoard} from "./board.js";
+import { buildGameBoard } from "./board.js";
 import Piece from "./piece.js";
 import { boardMap } from "./board.js";
 
@@ -9,15 +9,15 @@ const player = createBoardPiece(pieces.player, 'jogador');
 const boxes = [];
 
 function createBoardPiece(piecePosition, className) {
-   const piece = new Piece(piecePosition.x, piecePosition.y);
-   piece.insertElementInto(className, board);
-   return piece;
+    const piece = new Piece(piecePosition.x, piecePosition.y);
+    piece.insertElementInto(className, board);
+    return piece;
 }
 
-for (let i = 0; i < pieces.block.length; i++)  {
+for (let i = 0; i < pieces.block.length; i++) {
 
-   let piece = createBoardPiece(pieces.block [i], 'Block');
-   boxes.push(piece);
+    let piece = createBoardPiece(pieces.block[i], 'Block');
+    boxes.push(piece);
 }
 
 window.addEventListener("keydown", function (event) {
@@ -31,39 +31,41 @@ function findBoxAtPosition(position) {
     return boxes.find((caixa) => caixa.x === position.x && caixa.y === position.y);
 }
 
-console.log(findBoxAtPosition({x: 1, y: 2}));
-console.log(findBoxAtPosition({x: 4, y: 4}));
-console.log(findBoxAtPosition({x: 4, y: 5}));
-console.log(findBoxAtPosition({x: 5, y: 5}));
+console.log(findBoxAtPosition({ x: 1, y: 2 }));
+console.log(findBoxAtPosition({ x: 4, y: 4 }));
+console.log(findBoxAtPosition({ x: 4, y: 5 }));
+console.log(findBoxAtPosition({ x: 5, y: 5 }));
 
 
 function handlePieceMovement(keycode) {
-    const next = player.nextPosition(keycode);
-    
-    const foundPiece = findBoxAtPosition(next);
+    const nextPlayerposition = player.nextPosition(keycode);
+
+    const foundPiece = findBoxAtPosition(nextPlayerposition);
 
     if (foundPiece) {
+
         const boxNextPosition = foundPiece.nextPosition(keycode);
 
         if (verifyPosition(boxNextPosition) && !findBoxAtPosition(boxNextPosition)) {
+
             foundPiece.moveTo(boxNextPosition);
-            player.moveTo(next);
+            player.moveTo(nextPlayerposition);
         }
     }
     else {
-        if (verifyPosition(next)) {
-            player.moveTo(next);
+        if (verifyPosition(nextPlayerposition)) {
+            player.moveTo(nextPlayerposition);
         }
     }
 }
 
 function verifyPosition(position) {
-   let { x, y } = position;
+    let { x, y } = position;
 
 
-   if (x < 0 || x >= boardMap.length || y < 0 || y >= boardMap[0].length) {
-       return false; 
-   }
+    if (x < 0 || x >= boardMap.length || y < 0 || y >= boardMap[0].length) {
+        return false;
+    }
 
-   return boardMap[x][y] !== '#';
+    return boardMap[x][y] !== '#';
 }
